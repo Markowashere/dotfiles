8681975f2af9c4a14df1adf81c8efb4e3d5b5fde
Marko Tikkanen <martikkanen@outlook.com>
2024-02-10 14:46:31 +0200

    small fix

--- scripts/bak.sh
+++ scripts/bak.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
-
 # Define a list of file paths
 PATHS=(
-    "~/.zshrc"
-    "~/.p10k.zsh"
-    "~/Brewfile"
-    "~/.gitconfig"
-    "~/.gitconfig-work"
-    "~/.config/nvim"
-    "~/Library/Application\ Support/Code/User/settings.json"
-    "~/Library/Application\ Support/Code/User/keybindings.json"
+    ~/.zshrc
+    ~/.p10k.zsh
+    ~/Brewfile
+    ~/.gitconfig
+    ~/.gitconfig-work
+    ~/.config/nvim
+    ~/Library/Application\ Support/Code/User/settings.json
+    ~/Library/Application\ Support/Code/User/keybindings.json
 )
 
 # Define a rule to add .bak postfix to file names
-for path in $PATHS; do
+for path in "${PATHS[@]}"; do
     if [ -e "$path" ]; then
         mv "$path" "$path.bak"
+        echo "File or directory backed up: $path"
+    else
+        echo "File or directory not found: $path"
     fi
 done
